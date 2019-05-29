dbf7bc5f68439c55756e89dee6cb8bbed75f908d
Raphaël Thériault <raphael_theriault@outlook.com>
2019-05-29 14:45:57 -0400

    adb in not present in the PATH on Windows

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,12 +17,12 @@ MainWindow::MainWindow(QWidget *parent) :
     songsfolderPath = "";
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-    const int* deps = new int(std::system("where adb") + std::system("where java"));
+    const auto deps = new int(std::system("where java"));
 #else
-    const int* deps = new int(std::system("which dotnet adb java"));
+    const auto deps = new int(std::system("which dotnet adb java"));
 #endif
     if (*deps != 0) {
-        QMessageBox* dd = new QMessageBox;
+        auto dd = new QMessageBox;
         dd->setWindowTitle("Missing Dependencies");
         dd->setIcon(QMessageBox::Critical);
         dd->setText("It appears you are missing required dependencies. Make sure they are installed and that you launched the program from a CLI with a correct PATH.");
@@ -32,7 +32,7 @@ MainWindow::MainWindow(QWidget *parent) :
         delete deps;
     }
 
-    AgreementDialog* ad = new AgreementDialog;
+    auto ad = new AgreementDialog;
     if (ad->exec() == 0) {
         exit(EXIT_SUCCESS);
     } else {
