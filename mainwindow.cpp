aa44b8f433db48e49dc0c2dd43c0007f295f58ea
Raphaël Thériault <raphael_theriault@outlook.com>
2019-05-29 12:17:02 -0400

    Check dependencies

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+#include <QMessageBox>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "agreementdialog.h"
@@ -8,6 +10,20 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     setFixedSize(width(), height());
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+    const int deps = std::system("where adb") + std::system("where java");
+#else
+    const int deps = std::system("which mono adb java");
+#endif
+    if (deps != 0) {
+        QMessageBox dd;
+        dd.setWindowTitle("Missing Dependencies");
+        dd.setIcon(QMessageBox::Critical);
+        dd.setText("It appears you are missing required dependencies. Make sure they are installed and that you launched the program from a CLI with a correct PATH.");
+        dd.exec();
+        exit(EXIT_FAILURE);
+    }
 
     AgreementDialog ad;
     if (ad.exec() == 0) {
