30cbdb6b10bc53bc798fe03c629faa6798dcf6b2
Raphaël Thériault <raphael_theriault@outlook.com>
2019-05-29 14:17:54 -0400

    More pointers

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,22 +17,26 @@ MainWindow::MainWindow(QWidget *parent) :
     songsfolderPath = "";
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-    const int deps = std::system("where adb") + std::system("where java");
+    const int* deps = new int(std::system("where adb") + std::system("where java"));
 #else
-    const int deps = std::system("which dotnet adb java");
+    const int* deps = new int(std::system("which dotnet adb java"));
 #endif
-    if (deps != 0) {
+    if (*deps != 0) {
         QMessageBox* dd = new QMessageBox;
         dd->setWindowTitle("Missing Dependencies");
         dd->setIcon(QMessageBox::Critical);
         dd->setText("It appears you are missing required dependencies. Make sure they are installed and that you launched the program from a CLI with a correct PATH.");
         dd->exec();
         exit(EXIT_FAILURE);
+    } else {
+        delete deps;
     }
 
     AgreementDialog* ad = new AgreementDialog;
     if (ad->exec() == 0) {
         exit(EXIT_SUCCESS);
+    } else {
+        delete ad;
     }
 }
 
